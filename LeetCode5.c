700ad732470d7176ec37e1d3fa3e4642db4c6c03
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-04-07 16:43:51 +0530

    Merge two sorted array.

---
+++ LeetCode5.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n);
+int main()
+{
+    int nums1[]={1,2,3,0,0,0},nums2[]={3,4,5};
+    merge(nums1,6,3,nums2,3,3);
+    printf("\n");
+    return 0;
+}
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n)
+{
+    int i=m-1,j=n-1,k=nums1Size-1;
+    while(i>=0 && j>=0)
+    {
+        if(nums1[i]>=nums2[j])
+        {
+            nums1[k]=nums1[i];
+            i--;
+        }
+        else
+        {
+            nums1[k]=nums2[j];
+            j--;
+        }
+        k--;
+    }
+    while(j>=0)
+    {
+        nums1[k]=nums2[j];
+        j--;
+        k--;
+    }
+    while(i>=0)
+    {
+        nums1[k]=nums1[i];
+        i--;
+        k--;
+    }
+    for(i=0;i<m+n;i++)
+    {
+        printf("%d ",nums1[i]);
+    }
+}
